6d8c4bc136526f5986db15a891515f51db99fbd8
JaiPrakashPS <jaiprakash.ps2023it@sece.ac.in>
2025-01-30 17:54:02 +0530

    Changes

--- app.js
+++ app.js
@@ -11,13 +11,7 @@ import { errorMiddleware } from "./middleware/error.js";
 const app = express();
 config({ path: "./config/config.env" });
 
-app.use(
-  cors({
-    origin: [process.env.FRONTEND_URL],
-    method: ["GET", "POST", "DELETE", "PUT"],
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use(cookieParser());
 app.use(express.json());
