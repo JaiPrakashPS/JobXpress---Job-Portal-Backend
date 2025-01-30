069de5024d21931af7c429878f818b75efd94eb4
JaiPrakashPS <jaiprakash.ps2023it@sece.ac.in>
2025-01-30 21:31:26 +0530

    Changes

--- app.js
+++ app.js
@@ -8,20 +8,36 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import fileUpload from "express-fileupload";
 import { errorMiddleware } from "./middleware/error.js";
+
 const app = express();
 config({ path: "./config/config.env" });
 console.log("Allowed Frontend URL:", process.env.FRONTEND_URL);
 
+// Define allowed origins
+const allowedOrigins = [
+  "https://job-xpress-job-portal-frontend.vercel.app",
+  "https://job-xpress-job-portal-frontend-etor6okso.vercel.app"
+];
+
+// CORS configuration
 app.use(
   cors({
-    origin: "job-xpress-job-portal-frontend.vercel.app", 
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: "GET,POST,PUT,DELETE",
     allowedHeaders: "Content-Type,Authorization",
-    credentials: true
+    credentials: true, // To allow cookies and other credentials
   })
 );
 
-app.options('*', cors())
+// Handle preflight requests
+app.options('*', cors());
+
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -32,10 +48,13 @@ app.use(
     tempFileDir: "/tmp/",
   })
 );
+
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/job", jobRouter);
 app.use("/api/v1/application", applicationRouter);
+
 dbConnection();
 
 app.use(errorMiddleware);
+
 export default app;
